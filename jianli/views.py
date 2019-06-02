6f2acbcc2302cc8732cb81e0c09b2db2b78da83a
Taoorwell <taoorwell@gmail.com>
2019-06-02 19:24:47 +0800

    first cv git

---
+++ jianli/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import json
+
+
+# Create your views here.
+def index(request):    
+    with open('jianli/data.json', 'rb') as f:
+        data = json.load(f)
+    dic = {}
+    for k, v in data.items():
+        dic[k] = v
+    
+    return render(request, 'jianli/index.html', dic)
